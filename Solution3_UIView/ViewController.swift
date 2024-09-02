383d8a635fde713d909219735e9af1214cb66c07
NguyenLeHieu <nguyenhieu157380@gmail.com>
2024-09-03 00:22:38 +0700

    Initial Commit

---
+++ Solution3_UIView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Solution3_UIView
+//
+//  Created by Nguyễn Văn Hiếu on 3/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
